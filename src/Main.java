4ce2bc01fcc4dc97cd2327a5911d13113e4e8fbd
anders11 <anderslatif@gmail.com>
2016-09-19 14:16:21 +0200

    javafx start

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,13 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Main /*extends Application*/ {
+public class Main extends Application{
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-
-		//launch(args);
+		launch(args);
 	}
 
-/*	@Override
+	@Override
 	public void start(Stage primaryStage) throws Exception {
 
 		BorderPane root = new BorderPane();
@@ -19,11 +17,13 @@ public class Main /*extends Application*/ {
 
 
 
+
 		primaryStage.setMaximized(true);
-		//scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+		scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+		primaryStage.setTitle("Kino EmpireXP");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-	}*/
+	}
 
 
 }
