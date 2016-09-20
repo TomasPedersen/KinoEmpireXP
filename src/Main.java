632bde5c31e024aa92c0f9d5940f60a97ff46938
anders11 <anderslatif@gmail.com>
2016-09-20 08:49:32 +0200

    preparing for scenebuilder

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
@@ -13,16 +15,9 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		BorderPane root = new BorderPane();
-		Scene scene = new Scene(root);
-
-		ToggleButton toggleButton = new ToggleButton();
-		root.setCenter(toggleButton);
-
-		primaryStage.setMaximized(true);
-		scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
-		primaryStage.setTitle("Kino EmpireXP");
-		primaryStage.setScene(scene);
+		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+		primaryStage.setTitle("Hello World");
+		primaryStage.setScene(new Scene(root, 300, 275));
 		primaryStage.show();
 	}
 
