6221f68b20c53a642d15386d25fcaa6e5d79c424
anders11 <anderslatif@gmail.com>
2016-09-22 19:11:37 +0200

    gridpane of filmnodes

--- src/FilmOversigtController.java
+++ src/FilmOversigtController.java
@@ -1,13 +1,23 @@
+import controller.FilmNodesToFilmOversigt;
 import javafx.fxml.FXML;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 public class FilmOversigtController {
 
 
     @FXML
-    public void thisIsTheMethod() {
-        System.out.println("hello");
-    }
+    public void addMoviesToTheGridpane() {
+
+        FilmNodesToFilmOversigt filmNodes = new FilmNodesToFilmOversigt();
+        BorderPane root = (BorderPane) Main.root;
+        GridPane gridPane = (GridPane) root.lookup("#GRIDPANEID");  // please change the ID accordingly
+        gridPane = filmNodes.gridPaneOfFilmNodes();  // remember that this method will take localdate as parameter
+        // create a combobox or datepicker from which you can get the date
+
+        }
+
+
+
 
 }
--- src/controller/FilmNodesToFilmOversigt.java
+++ src/controller/FilmNodesToFilmOversigt.java
@@ -7,14 +7,47 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import model.DBHelper;
 import model.Film;
 
+import java.sql.ResultSet;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class FilmNodesToFilmOversigt {
 
 
-    public ArrayList<Node> createListOfNodes(ArrayList<Film> films){
+
+    public GridPane gridPaneOfFilmNodes() {
+        GridPane gridPane = new GridPane();
+
+        // Localdate from combobox or datepicker
+        LocalDate date = LocalDate.now();  // change this and give this method a parameter
+
+        int columnIndex = 0;
+        int rowIndex = 0;
+        for (Node filmNode : createListOfFilmNodes(date)) {
+
+            if(columnIndex == 2) {
+                columnIndex = 0;
+                rowIndex++;
+            }
+            gridPane.add(filmNode, columnIndex, rowIndex);
+            columnIndex++;
+
+        }
+        return gridPane;
+    }
+
+
+
+
+
+    public ArrayList<Node> createListOfFilmNodes(LocalDate date){
+        ArrayList<Film> films = new ArrayList<>();
+        DBHelper db = new DBHelper();
+        // if only I could do this : db.selectFromMovies(null, null)  and get an arraylist of films
+        // this method does not work for dates
 
         ArrayList<Node> listOfNodes = new ArrayList<>();
 
@@ -36,10 +69,7 @@ public class FilmNodesToFilmOversigt {
         return listOfNodes;
     }
 
-    public void positionNodesInGridPane(ArrayList<Node> nodeList, GridPane gridPane){
 
-
-    }
 
 
 }
