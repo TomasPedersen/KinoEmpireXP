b2e09b6822dfb0e9494734401c4779f3421d533b
anders11 <anderslatif@gmail.com>
2016-09-22 10:06:56 +0200

    models

--- src/controller/Reservation.java
+++ src/controller/Reservation.java
@@ -1,23 +1,51 @@
+/*
 package controller;
 
+import model.Film;
 import model.Seat;
+import model.Show;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Reservation {
 
-    ArrayList<Seat> lilleSal = new ArrayList<>();
-    ArrayList<Seat> storSal = new ArrayList<>();
+
+    public ArrayList<Seat> createShowLilleSal(Film film, LocalDate date) {
+
+        Show show = new Show(film, date);
+        ArrayList<Seat> seats = show.getLilleSalSeatList();
+        */
+/*SELECT * FROM LOG WHERE DATA = SELECTEDDATE && WHERE SEAT=i*//*
+
+        for (int i = 0; i < 240; i++) {
+            if(*/
+/*NOW WE CHECK IF a seat is available*//*
+) {
+                Seat seat = new Seat(true); // is reserved true
+            } else {
+                Seat seat = new Seat(false);
+            }
+        }
+    }
 
 
-    public void checkIfReserved(int amountOfSeats) {
+    public ArrayList<Seat> createShowBigSal(Film film, LocalDate date) {
 
-        // amountOfSeats smallest theater = 240
-        // amountOfSeats smallest theater = 400
 
-        for (int i = 0; i < amountOfSeats; i++) {
-            if(/*NOW WE CHECK IF */) {
+        Show show = new Show(film, date);
+        ArrayList<Seat> seats = show.getLilleSalSeatList();
+        */
+/*SELECT * FROM LOG WHERE DATA = SELECTEDDATE && WHERE SEAT=i*//*
 
+        for (int i = 0; i < 400; i++) {
+            if(*/
+/*NOW WE CHECK IF a seat is available*//*
+) {
+                Seat seat = new Seat(true); // is reserved true
+                seats.add(seat);
+            } else {
+                Seat seat = new Seat(false);
             }
         }
 
@@ -26,3 +54,4 @@ public class Reservation {
 
 
 }
+*/
