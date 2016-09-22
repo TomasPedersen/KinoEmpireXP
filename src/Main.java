ab48e5ed63fa289f42c5efcf1d23ec160525b6f0
anders11 <anderslatif@gmail.com>
2016-09-22 02:14:59 +0200

    I can't sleep

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import model.DBHelper;
 
 public class Main extends Application{
 
