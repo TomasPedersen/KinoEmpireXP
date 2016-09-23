b294d36886020430276e9c4551e83e0117c0fc04
Marc Obel <marcentusch@hotmail.com>
2016-09-23 10:32:08 +0200

    .

--- src/FilmOversigtController.java
+++ src/FilmOversigtController.java
@@ -13,7 +13,7 @@ public class FilmOversigtController {
 
         FilmNodesToFilmOversigt filmNodes = new FilmNodesToFilmOversigt();
         BorderPane root = (BorderPane) Main.root;
-        GridPane gridPane = (GridPane) root.lookup("#GRIDPANEID");  // please change the ID accordingly
+        GridPane gridPane = (GridPane) root.lookup("#gridInFilmOversigt");  // please change the ID accordingly
         gridPane = filmNodes.gridPaneOfFilmNodes();  // remember that this method will take localdate as parameter
         // create a combobox or datepicker from which you can get the date
 
