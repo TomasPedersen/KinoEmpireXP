aaf794a9add71e6a97f877ff1ab7e8187c355474
DRChrist <dennis_christensen87@hotmail.com>
2016-09-23 11:09:17 +0200

    Ændringer i DBHelper og script.

--- src/controller/MovieProgram.java
+++ src/controller/MovieProgram.java
@@ -13,19 +13,19 @@ public class MovieProgram {
     public void createProgramForTheNextThreeMonths() {
 
         DBHelper db = new DBHelper();
-        Movie movie1 = new Movie("Haha, you're dead!", null, "Comedy", 123, "For the whole family.", null, 80, "Mr. Film Director", 0, null);
+        Movie movie1 = new Movie("Haha, you're dead!", null, "Comedy", 123, "For the whole family.", null, 80, "Mr. Movie Director", 0, null);
         Show show1 = new Show(movie1, null, 1000, 0);
         Show show2 = new Show(movie1, null, 1300, 0);
         Show show3 = new Show(movie1, null, 1600, 0);
         Show show4 = new Show(movie1, null, 2000, 0);
 
-        Movie movie2 = new Movie("Gotta save the world.", null, "Action", 123, "A retired man is asked by the president to save the world. And he does.", null, 80, "Mr. Film Director", 0, null);
+        Movie movie2 = new Movie("Gotta save the world.", null, "Action", 123, "A retired man is asked by the president to save the world. And he does.", null, 80, "Mr. Movie Director", 0, null);
         Show show5 = new Show(movie2, null, 1000, 0);
         Show show6 = new Show(movie2, null, 1300, 0);
         Show show7 = new Show(movie2, null, 1600, 0);
         Show show8 = new Show(movie2, null, 2000, 0);
 
-        Movie movie3 = new Movie("Ghost Nuns 2", null, "Horror", 123, "Diapers will be provided.", null, 80, "Mr. Film Director", 16, null);
+        Movie movie3 = new Movie("Ghost Nuns 2", null, "Horror", 123, "Diapers will be provided.", null, 80, "Mr. Movie Director", 16, null);
         Show show9 = new Show(movie3, null, 1900, 0);
         Show show11 = new Show(movie3, null, 2200, 0);
         Show show12 = new Show(movie3, null, 2300, 0);
--- src/model/Movie.java
+++ src/model/Movie.java
@@ -2,7 +2,7 @@ package model;
 
 import java.time.LocalDate;
 
-public class Film {
+public class Movie {
 
     private String danishTitle;
     private String originalTitle;
@@ -15,7 +15,7 @@ public class Film {
     private int ageRestriction;
     private String versions; // language, subtitles and 3d
 
-    public Film(String danishTitle, String originalTitle, String genre, double filmLength, String filmDescription, LocalDate releaseDate, double price, String director, int ageRestriction, String versions) {
+    public Movie(String danishTitle, String originalTitle, String genre, double filmLength, String filmDescription, LocalDate releaseDate, double price, String director, int ageRestriction, String versions) {
         this.danishTitle = danishTitle;
         this.originalTitle = originalTitle;
         this.Genre = genre;
