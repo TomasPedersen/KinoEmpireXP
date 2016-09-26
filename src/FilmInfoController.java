7a00182c3f7c55c426d71f5e76e3c5454ae31a7e
Marc Obel <marcentusch@hotmail.com>
2016-09-26 06:04:28 +0200

    FilmInfo controller og filmInfo scene stuff

--- src/FilmInfoController.java
+++ src/FilmInfoController.java
@@ -1,9 +1,18 @@
-/**
- * Created by Marc on 25-09-2016.
- */
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+
 public class FilmInfoController {
 
+
+    //SceneBuilder can't find this class for controller.
+    //Kan ikke få lortet til at virke.
+
+    /*@FXML
     public void initializeWebView(){
+        WebView descriptionWebview = (WebView) Main.root.lookup("#description_webview");
+        description_webview.getEngine().load("");
+
 
     }
+    */
 }
